47980f57ea9a1c41fcc910ebdc87861c68857185
Carl Corsini <carlcorsin@yahoo.com>
2021-05-11 13:22:19 -0700

    did it

--- src/Home.js
+++ src/Home.js
@@ -32,11 +32,13 @@ const { MediaContextProvider, Media } = createMedia({
  * components for such things.
  */
 const HomepageHeading = ({ mobile }) => (
-  <Container id="home-heading" style text>
+  <Container
+    className="opaque"
+    style={{ borderRadius: '15px', paddingBottom: '1em' }}
+    text>
     <Header
-    
       as="h1"
-      content="The Wedding of Estefany And Carl"
+      content="Estefany & Carl"
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
@@ -47,21 +49,23 @@ const HomepageHeading = ({ mobile }) => (
     />
     <Header
       as="h2"
-      content="June 5th, 2021"
+      content="The Wedding of Estefany & Carl"
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
         fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
       }}
     />
-    <Button
-      href="https://www.zola.com/registry/carlandestefany"
-      primary
-      size="huge">
-      Wedding Registry
-      <Icon name="right arrow" />
-    </Button>
+    <Header
+      as="h5"
+      content="June 5th, 2021"
+      inverted
+      style={{
+        fontSize: mobile ? '1em' : '1.2em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '1em' : '1.2em',
+      }}
+    />
   </Container>
 );
 
@@ -90,16 +94,53 @@ class DesktopContainer extends Component {
           onBottomPassed={this.showFixedMenu}
           onBottomPassedReverse={this.hideFixedMenu}>
           <Segment
+            className="home-heading"
             inverted
             textAlign="center"
-            style={{ minHeight: 700, padding: '1em 0em' }}
+            style={{ minHeight: 1000, padding: '1em 0em' }}
             vertical>
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size="large"></Menu>
+            <div className="opaque">
+              <Menu
+                fixed={fixed ? 'top' : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size="large">
+                <Container>
+                  <Menu.Item position="right">
+                    <Button
+                      as="a"
+                      inverted
+                      color="green"
+                      size="huge"
+                      href="https://www.zola.com/registry/carlandestefany"
+                      target="_blank"
+                      animated>
+                      <Button.Content visible>Wedding Registry</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name="gift" />
+                      </Button.Content>
+                    </Button>
+                    <Button
+                      href="#details"
+                      inverted={!fixed}
+                      style={{ marginLeft: '0.5em' }}>
+                      Details
+                    </Button>
+                    <Button
+                      href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3A5270f9355e0849bdbac75b2b7971e74a%40thread.tacv2%2F1620184307481%3Fcontext%3D%257b%2522Tid%2522%253a%2522a56162b0-44e1-44ff-b810-b703aa81c222%2522%252c%2522Oid%2522%253a%2522cfed5b4a-cf8f-4d16-8227-7855007877e6%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=c9c842e9-ca5b-48b9-89a2-3f5405d2c298&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
+                      target="_blank"
+                      as="a"
+                      inverted={!fixed}
+                      primary={fixed}
+                      style={{ marginLeft: '0.5em' }}>
+                        <Icon name="microsoft"/>
+                    Join Live Meeting
+                    </Button>
+                  </Menu.Item>
+                </Container>
+              </Menu>
+            </div>
             <HomepageHeading />
           </Segment>
         </Visibility>
@@ -125,7 +166,75 @@ class MobileContainer extends Component {
     const { children } = this.props;
     const { sidebarOpened } = this.state;
 
-    return <Media as={Sidebar.Pushable} at="mobile"></Media>;
+    return (
+      <Media as={Sidebar.Pushable} at="mobile">
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={sidebarOpened}>
+            <Menu.Item href="#details" as="a">
+              Details
+            </Menu.Item>
+            <Menu.Item
+              href="https://www.zola.com/registry/carlandestefany"
+              as="a">
+              Registry
+            </Menu.Item>
+            <Menu.Item
+              href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3A5270f9355e0849bdbac75b2b7971e74a%40thread.tacv2%2F1620184307481%3Fcontext%3D%257b%2522Tid%2522%253a%2522a56162b0-44e1-44ff-b810-b703aa81c222%2522%252c%2522Oid%2522%253a%2522cfed5b4a-cf8f-4d16-8227-7855007877e6%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=c9c842e9-ca5b-48b9-89a2-3f5405d2c298&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
+              target="_blank"
+              as="a">
+                <Icon name="microsoft"/>
+            Join Live Meeting
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Segment
+              className="home-heading"
+              inverted
+              textAlign="center"
+              style={{ minHeight: 250, padding: '1em 0em' }}
+              vertical>
+              <Container className="opaque">
+                <Menu inverted pointing secondary size="large">
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name="sidebar" />
+                  </Menu.Item>
+                  <Menu.Item >
+                    <Button
+                      href="https://www.zola.com/registry/carlandestefany"
+                      target="_blank"
+                      as="a"
+                      inverted>
+                      <Icon name="gift" />
+                      Registry
+                    </Button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Button
+                      href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3A5270f9355e0849bdbac75b2b7971e74a%40thread.tacv2%2F1620184307481%3Fcontext%3D%257b%2522Tid%2522%253a%2522a56162b0-44e1-44ff-b810-b703aa81c222%2522%252c%2522Oid%2522%253a%2522cfed5b4a-cf8f-4d16-8227-7855007877e6%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=c9c842e9-ca5b-48b9-89a2-3f5405d2c298&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
+                      target="_blank"
+                      as="a"
+                      inverted>
+                      <Icon name="microsoft" />
+                      Teams
+                    </Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomepageHeading mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Media>
+    );
   }
 }
 
@@ -154,98 +263,46 @@ const HomepageLayout = () => (
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
+            <Header
+              id="details"
+              as="h2"
+              style={{ fontSize: '2em', fontStyle: 'italic' }}>
+              Wedding Details
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never
-              thought possible. Let us delight your customers and empower your
-              needs... through pure data analytics.
+            <p style={{ fontSize: '1.75em' }}>
+              Save the rapidly approaching date of June 5th, 2021.
+            </p>
+            <p style={{ fontSize: '1.5em' }}>
+              Estefany Rodriguez Solano and Carl Corsini are getting married
+              Saturday, June 5th, 2021 @ 4pm
             </p>
             <Header as="h3" style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
+              Location
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even
-              bananas can be bioengineered.
-            </p>
+            <p style={{ fontSize: '1.5em' }}>The Ranch</p>
+            <p style={{ fontSize: '1.33em' }}>25 Nelson Lane</p>
+            <p style={{ fontSize: '1.33em' }}>Cotati, California</p>
           </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="/images/wireframe/white-image.png"
-            />
+          <Grid.Column floated="right" width={8}>
+            <Header as="h3" style={{ fontSize: '2em' }}>
+              Timing
+            </Header>
+            <p style={{ fontSize: '1.5em' }}>Guests Arrive: 3pm</p>
+            <p style={{ fontSize: '1.5em' }}>Ceremony: 4pm</p>
+            <Header as="h3" style={{ fontSize: '2em' }}>
+              Attire
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>Flats recommended</p>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
-          </Grid.Column>
+          <Container>
+            <Image
+              style={{ marginTop: '2em' }}
+              src="https://github.com/carlcorsini/wedding/blob/main/src/pics/The%20Wedding%20of%20Estefany%20&%20Carl.png?raw=true"></Image>
+          </Container>
         </Grid.Row>
       </Grid>
-    </Segment>
-
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled="internally" columns="equal" stackable>
-        <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              That is what they all say about us
-            </p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as="h3" style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
-        </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
-
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}>
-          <a href="#">Case Studies</a>
-        </Divider>
-
-        <Header as="h3" style={{ fontSize: '2em' }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur
-          filler content, but it's really true. It took years of gene splicing
-          and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as="a" size="large">
-          I'm Still Quite Interested
-        </Button>
-      </Container>
     </Segment>
   </ResponsiveContainer>
 );
